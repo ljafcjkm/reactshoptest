ba61cf3622834e058f4214d00c3019594635fea3
ljafcjkm <52883998+ljafcjkm@users.noreply.github.com>
2020-09-14 22:33:57 +0300

    first test

---
+++ precache-manifest.c90805518c33dfc5d474bfeea086cf8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab391789a5a2e55b4e86a40dee44d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a27ecbcbf4cc269c0d34",
+    "url": "/static/css/main.cc1a99ee.chunk.css"
+  },
+  {
+    "revision": "248b14de025a45cf4eac",
+    "url": "/static/js/2.a3d089c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a3d089c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27ecbcbf4cc269c0d34",
+    "url": "/static/js/main.dcbbeb2d.chunk.js"
+  },
+  {
+    "revision": "e5c6b10368c19e12c132",
+    "url": "/static/js/runtime-main.eccc1d1e.js"
+  }
+]);
